9e8b359264b77b61a842f5920b67676d3b5bd79a
smpineda129 <smpineda129@gmail.com>
2023-04-06 19:56:08 -0500

    Creacion de slider

--- scripts/script-home.js
+++ scripts/script-home.js
@@ -1,1 +1,21 @@
-console.log("hola")
+(function(){
+    const sliders = [...document.querySelectorAll('.slider-body')];
+    const arrowNext = document.querySelector('#next');
+    const arrowPrev = document.querySelector('#prev');
+    let value;
+
+    arrowNext.addEventListener("click" , () => changePosition(1));
+    arrowPrev.addEventListener("click" , () => changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector('.slider-body-show').dataset.id);
+        value = currentElement;
+        value += change;
+        if (value === 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[currentElement-1].classList.toggle("slider-body-show");
+        sliders[value-1].classList.toggle("slider-body-show");
+    }
+})()
